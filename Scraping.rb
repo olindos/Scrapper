40e1afa581f5dd3b76e9c240ec6cbc6b4ae8846a
olindos <yason4ik87@gmail.com>
2018-10-23 11:53:27 +0300

    Refractoring scrapper

--- Scraping.rb
+++ Scraping.rb
@@ -3,80 +3,33 @@ require 'nokogiri'
 
 class Scraper
 
-attr_accessor :parse_page
-  
   def perform
-    url = 'https://twitter.com/'
-    doc = HTTParty.get(url)
-    @parse_page ||= Nokogiri::HTML(doc)
-  end
+    parsed_content = []
+    parsed_tags = ["button", "div", "link", "span"]
 
-  def get_names
-    names = perform.css("title")
-    names.each do |name|
-      a = { name.name => {name.text => {type: name.name, selector: "css", identifier: name.css_path }}}
-      puts a
+    parsed_tags.each do |parsed_tag|
+      url = 'https://twitter.com/'
+      doc = HTTParty.get(url)
+      parse_page ||= Nokogiri::HTML(doc)
+      parse_page.css(parsed_tag).each do |tag|
+        if parsed_tag == "div"
+          parsed_content << { tag.name => {tag['id'] => {type: tag.name, selector: "css", class: tag['class'],
+            identifier: tag.css_path }}}
+        elsif parsed_tag == "link"
+          parsed_content << { tag.name => { tag['title'] => {type: tag['rel'], selector: "css", link: tag['href'],
+            identifier: tag.css_path }}}
+        elsif parsed_tag == "span"
+          parsed_content << { tag.name => {tag.text => {type: tag.name, selector: "css", class: tag['class'],
+            identifier: tag.css_path }}}
+        else
+          parsed_content << { tag.name => {tag.text => {type: tag.name, selector: "css", identifier: tag.css_path }}}
+        end
+      end
     end
-  end
-
-  def get_button
-    buttons = perform.css("button")
-    buttons.each do |button|
-      a = { button.name => {button.text => {type: button.name, selector: "css", identifier: button.css_path }}}
-      puts a
-    end
-  end
-
-  def get_span
-    spans = perform.css("span")
-    spans.each do |span|
-      a = { span.name => {span.text => {type: span.name, selector: "css", class: span['class'],
-        identifier: span.css_path }}}
-      puts a
-    end
-  end
-
-  def get_link
-    links = perform.css("link")
-    links.each do |link|
-      a = { link.name => {link['title'] => {type: link['rel'], selector: "css", link: link['href'],
-        identifier: link.css_path }}}
-      puts a
-    end
-  end
-
-  def get_select_list
-    lists = perform.css("select_list")
-    puts lists
-  end
-
-  def get_checkbox
-    checkboxs = perform.css("checkbox")
-    puts checkboxs
-  end
-
-  def get_radio
-    radios = perform.css("radio")
-    puts radios
-  end
-
-  def get_div
-    divs = perform.css("div")
-    divs.each do |div|
-      a = { div.name => {div['id'] => {type: div.name, selector: "css", class: div['class'],
-        identifier: div.css_path }}}
-      puts a
-    end
+    puts parsed_content
   end
 
   scraper = Scraper.new
-  name = scraper.get_names
-  button = scraper.get_button
-  span = scraper.get_span
-  link = scraper.get_link
-  select_list = scraper.get_select_list
-  checkbox = scraper.get_checkbox
-  radio = scraper.get_radio
-  div = scraper.get_div
-  
+  puts scraper.perform
+
 end
